c844f314346b78acdb08dc8b4dff15c67b9de5c6
Jon Butt <jon.d.butt@gmail.com>
2022-12-15 00:20:18 -0800

    Add API_URL enum

--- src/data.py
+++ src/data.py
@@ -3,8 +3,8 @@
 from enum import Enum
 
 
-class team(Enum):
-    """An enum representing a team. Value is the team id"""
+class Team(Enum):
+    """Enumerate current NHL teams. The value is a string representation of the team's ID in the API"""
 
     ANA = "24"
     ARI = "53"
@@ -40,17 +40,52 @@ class team(Enum):
     WSH = "15"
 
 
-class conference(Enum):
+class Conference(Enum):
     """An enum representing a conference"""
 
     eastern = "6"
     western = "5"
 
 
-class division(Enum):
+class Division(Enum):
     """An enum representing a division"""
 
     atlantic = "17"
     metropolitan = "18"
     pacific = "15"
     central = "16"
+
+
+class API_URL_root(Enum):
+    """an enumeration of the NHL api URL root"""
+
+    stats = "https://statsapi.web.nhl.com/api/v1"
+    records = "https://records.nhl.com/site/api"
+
+
+class API_URL(Enum):
+    """base urls for api endpoints. Many of these take modifiers to refine the query."""
+
+    play_by_play = f"{API_URL_root.stats.value}/game/ID/feed/live"
+    schedule = f"{API_URL_root.stats.value}/schedule"
+    standings = f"{API_URL_root.stats.value}/standings"
+    franchise = f"{API_URL_root.stats.value}/franchises"
+    teams = f"{API_URL_root.stats.value}/teams"
+    division = f"{API_URL_root.stats.value}/divisions"
+    conference = f"{API_URL_root.stats.value}/conferences"
+    people = f"{API_URL_root.stats.value}/people"
+    game_types = f"{API_URL_root.stats.value}/gameTypes"
+    game_status = f"{API_URL_root.stats.value}/gameStatus"
+    play_types = f"{API_URL_root.stats.value}/playTypes"
+    tournament_types = f"{API_URL_root.stats.value}/tournamentTypes"
+    playoffs = f"{API_URL_root.stats.value}/tournaments/playoffs"
+    seasons = f"{API_URL_root.stats.value}/seasons"
+    current_season = f"{API_URL_root.stats.value}/seasons/current"
+    standing_types = f"{API_URL_root.stats.value}/standingsTypes"
+    stats_types = f"{API_URL_root.stats.value}/statTypes"
+    draft = f"{API_URL_root.stats.value}/draft"
+    prospects = f"{API_URL_root.stats.value}/prospects"
+    awards = f"{API_URL_root.stats.value}/awards"
+    venues = f"{API_URL_root.stats.value}/venues"
+    event_types = f"{API_URL_root.stats.value}/eventTypes"
+    performer_types = f"{API_URL_root.stats.value}/performerTypes"
